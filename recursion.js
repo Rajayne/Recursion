9b82827b9b1feddbd1c2b52764c8ef0959726e80
Rajayne <ryann.j.nealey@gmail.com>
2023-05-30 16:04:16 -0700

    Create everyOther function and pass test

--- recursion.js
+++ recursion.js
@@ -7,11 +7,20 @@ function product(nums, i = 0) {
 
 /** longest: return the length of the longest word in an array of words. */
 
-function longest(words) {}
+function longest(words) {
+  //Base Case
+  if (!words.length) return 0;
+  // Recursive Case
+  let longestWord = longest(words.slice(1));
+  return Math.max(longestWord, words[0].length);
+}
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {}
+function everyOther(str, i = 0) {
+  if (i >= str.length) return "";
+  return str[i] + everyOther(str, i + 2);
+}
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
@@ -44,3 +53,8 @@ module.exports = {
   gatherStrings,
   binarySearch,
 };
+
+console.log(longest(["hello", "hi", "hola"])); // 5
+console.log(longest(["abcdefg", "hijklmnop", "qrs", "tuv", "wx", "y", "z"])); // 9
+console.log(longest(["a", "b", "c", "d", "e"])); // 1
+console.log(longest(["abcde"])); //5
